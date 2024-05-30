44bf562311b17f7b4531159daf97f44bc0704e87
Luis Angel Segura <angeldevmx27@gmail.com>
2024-05-30 14:12:54 -0600

    Clses de hoy sobre import/ export

---
+++ Clase-15/main.js
@@ -0,0 +1,9 @@
+import{area} from './funciones.js'
+import { perimetro } from './funciones.js';
+import {PI} from './funciones.js'
+
+console.log('area: ' + area(10));
+console.log('perimetro: ' + perimetro(10));
+
+console.log('Por si no lo sabes Pi es igual a: ' + PI);
+
